44bc403facfb1b09ac7f5793905259187a6c38d3
gurasan0110 <gurasan0110@icloud.com>
2024-12-01 17:52:57 +0900

    環境変数の見直し

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:qiita/app.dart';
 
-Future<void> main() async {
-  await dotenv.load();
-
+void main() {
   runApp(ProviderScope(child: const App()));
 }
