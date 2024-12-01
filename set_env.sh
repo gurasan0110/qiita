e890600d310c353ce29584edf33f7f3ea1b81401
gurasan0110 <gurasan0110@icloud.com>
2024-12-01 16:16:04 +0900

    環境変数に対応

---
+++ set_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Client IDを入力してください: " CLIENT_ID
+read -p "Client Secretを入力してください: " CLIENT_SECRET
+
+cat << EOF > .env
+CLIENT_ID='$CLIENT_ID'
+CLIENT_SECRET='$CLIENT_SECRET'
+EOF
