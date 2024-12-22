92e2a497049e80b3fd1d19fcaaec1091b224fe8f
gurasan0110 <gurasan0110@icloud.com>
2024-12-22 16:34:36 +0900

    GitHub Actionsに対応

--- setup_env.sh
+++ setup_env.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-read -p 'Client IDを入力してください: ' CLIENT_ID
-read -p 'Client Secretを入力してください: ' CLIENT_SECRET
-
 cat << EOF > .env
-CLIENT_ID='$CLIENT_ID'
-CLIENT_SECRET='$CLIENT_SECRET'
+CLIENT_ID='$1'
+CLIENT_SECRET='$2'
 EOF
 
 echo '環境変数をセットアップ'
